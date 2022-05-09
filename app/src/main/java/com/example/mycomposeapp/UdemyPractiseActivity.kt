c8ae361db57dec7c2c834b2b78b504229811e93c
Rohit Pareekh <pareek.rohit95@gmail.com>
2022-05-09 13:20:20 +0530

    Udemy Compose activity

--- app/src/main/java/com/example/mycomposeapp/UdemyPractiseActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/UdemyPractiseActivity.kt
@@ -3,13 +3,17 @@ package com.example.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class UdemyPractiseActivity : ComponentActivity() {
@@ -31,7 +35,13 @@ class UdemyPractiseActivity : ComponentActivity() {
 
 @Composable
 fun Greeting3(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hi there!"
+    , modifier = Modifier
+            .border(2.dp, Color.Green)  /**this will be set in outer layer and then goes to inner one*/
+            .padding(50.dp)
+            .border(2.dp, Color.Red)
+            .padding(50.dp),
+    )
 }
 
 @Preview(showBackground = true)
